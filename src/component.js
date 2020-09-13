e4da6ba353d61314ebe7c568c72ba8523a7ae025
Bruno-Cerqueira <brunocerqueirasd@gmail.com>
2020-09-13 19:54:26 -0300

    Add title js lazy load file.

--- src/component.js
+++ src/component.js
@@ -3,5 +3,14 @@ export default (text = "Hello world") => {
   element.className = "rounded bg-red-100 border max-w-md m-4 p-4 alm-font";
 
   element.innerHTML = text;
+  element.onclick = () =>
+    import("./title")
+      .then((title) => {
+        element.textContent = title.default;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
   return element;
 };
